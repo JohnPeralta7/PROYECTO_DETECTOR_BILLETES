b9bd924c398cbbfd09ad5a02c3f9719aca4fb468
Angel0956 <angel.1947567@gmail.com>
2025-10-13 18:49:24 -0500

    frontend

---
+++ billete/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'billete'
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('billetes/', views.detectar_billetes, name='billetes'),
+    path('monedas/', views.detectar_monedas, name='monedas'),
+]
--- billete/views.py
+++ billete/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    """Vista para la página de inicio"""
+    return render(request, 'billete/inicio.html')
+
+def detectar_billetes(request):
+    """Vista para detectar billetes"""
+    return render(request, 'billete/billetes.html')
+
+def detectar_monedas(request):
+    """Vista para detectar monedas"""
+    return render(request, 'billete/monedas.html')
+
+
+
