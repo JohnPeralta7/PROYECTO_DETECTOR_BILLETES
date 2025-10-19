445ea7edf34dec554df34ed6f37b462199121d31
kfrancor <karenfr397@gmail.com>
2025-10-18 23:40:41 -0500

    creacion de modelos

--- billete/models.py
+++ billete/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class año_mision(models.Model):
+    año = models.CharField(max_length=5)
+    serie_letra = models.CharField(max_length=2)
+    
+    def __str__(self):
+        return f"{self.año} - {self.serie_letra}"
+    
+class bancos(models.Model):
+    indicador = models.CharField(max_length=10)
+    banco = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.nombre} ({self.indicador})"
